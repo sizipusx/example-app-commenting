22058c5a62646bd62b5f37bfc74cbfd7eba8dd6f
sizipusx <76734279+sizipusx@users.noreply.github.com>
2021-12-20 15:55:58 +0900

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,6 @@
 SCOPE = "https://www.googleapis.com/auth/spreadsheets"
-SPREADSHEET_ID = "1rkMVLvh3JrBq_tbi4Ho0qjCDAP3vYdNuWOEjYpkJLNU"
-SHEET_NAME = "Database"
+SPREADSHEET_ID = "1-seXiY5a1NuMNwURA409Q6u2g8ZQo843xMJku7mV6Q0"
+SHEET_NAME = "격언"
 GSHEET_URL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}"
 COMMENT_TEMPLATE_MD = """{} - {}
 > {}"""
