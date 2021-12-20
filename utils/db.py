980abf3bd65db40215aaa9f1f38cadce40d8f983
sizipusx <76734279+sizipusx@users.noreply.github.com>
2021-12-20 15:49:15 +0900

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -4,8 +4,9 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
 SCOPE = "https://www.googleapis.com/auth/spreadsheets"
-SPREADSHEET_ID = "1rkMVLvh3JrBq_tbi4Ho0qjCDAP3vYdNuWOEjYpkJLNU"
-SHEET_NAME = "Database"
+#SPREADSHEET_ID = "1rkMVLvh3JrBq_tbi4Ho0qjCDAP3vYdNuWOEjYpkJLNU"
+SPREADSHEET_ID = "1-seXiY5a1NuMNwURA409Q6u2g8ZQo843xMJku7mV6Q0"
+SHEET_NAME = "격언"
 GSHEET_URL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}"
 
 
