55c270b63efa93e2673bdf68868412d49459b01b
sizipusx <76734279+sizipusx@users.noreply.github.com>
2021-12-20 16:57:45 +0900

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -6,7 +6,7 @@ from googleapiclient.discovery import build
 SCOPE = "https://www.googleapis.com/auth/spreadsheets"
 #SPREADSHEET_ID = "1rkMVLvh3JrBq_tbi4Ho0qjCDAP3vYdNuWOEjYpkJLNU"
 SPREADSHEET_ID = "1-seXiY5a1NuMNwURA409Q6u2g8ZQo843xMJku7mV6Q0"
-SHEET_NAME = "격언"
+SHEET_NAME = "일지기록"
 GSHEET_URL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}"
 
 
